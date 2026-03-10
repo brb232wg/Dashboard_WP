ffb2f05ad401159c5e32f9d422cfe4a8c55763e3
corru <corruptededitor@gmail.com>
2026-03-10 13:38:57 +0000

    Reset main to remove map

--- api/global-monitor/markets.js
+++
@@ -1,8 +0,0 @@
-import { getSnapshot } from '../../lib/global-monitor/eventStore.js';
-import { ensureIngestorsStarted } from '../../lib/global-monitor/ingestors/startIngestors.js';
-
-export default function handler(_req, res) {
-  ensureIngestorsStarted();
-  const snapshot = getSnapshot();
-  res.status(200).json(snapshot.markets || {});
-}
--- api/global-monitor/summary.js
+++
@@ -1,8 +0,0 @@
-import { getSummary } from '../../lib/global-monitor/eventStore.js';
-import { ensureIngestorsStarted } from '../../lib/global-monitor/ingestors/startIngestors.js';
-
-export default function handler(req, res) {
-  ensureIngestorsStarted();
-  const { category = 'all' } = req.query || {};
-  res.status(200).json(getSummary({ category }));
-}
